6ea5545ec6cb8040df142ad00ea96c5ce9d4cf4e
Mejba Hasan <mail.mejbahasan@gmail.com>
2025-06-04 21:40:18 +0600

    check dynamic typing

--- src/components/body/Hero.jsx
+++ src/components/body/Hero.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
 import Button from "../../assets/Button";
 
 const Hero = () => {
+  const [typing, setTyping] = useState('');
+  const [arryindex, setarryindex] = useState(0);
+
+  useEffect(() => {
+    const text = ["Mejba Hasan", "A Frontend Developer", "A Backend Developer"];
+    let i = -1;
+
+    const typeInterval = setInterval(() => {
+      if (i < text[arryindex].length - 1) {
+        i++;
+        setTyping((prev) => prev + text[arryindex][i]);
+      }
+    }, 70);
+
+    const resetTimeout = setTimeout(() => {
+      clearInterval(typeInterval);
+      deleteText();
+    }, 4000);
+
+    const deleteText = () => {
+      const deleteInterval = setInterval(() => {
+        if (i >= 0) {
+          setTyping((prev) => prev.slice(0, -1));
+          i--;
+        } else {
+          clearInterval(deleteInterval);
+          setarryindex((prevIndex) => (prevIndex + 1) % text.length);
+        }
+      }, 100);
+    };
+
+    // Cleanup the intervals and timeout when the component unmounts
+    return () => {
+      clearInterval(typeInterval);
+      clearTimeout(resetTimeout);
+    };
+  }, [arryindex]);
   return (
     <Main id="home">
       <div className="content">
-        <h4 className="text-7 fw-medium">Welcome</h4>
-        <h1 className=" fw-semibold text-16 py-lg-2">I am Mejba Hasan</h1>
+        <h4 className="text-7 fw-medium mb-3">Welcome</h4>
+        <h1 className=" fw-semibold text-16">I am <span className="text-primary">{typing}</span></h1>
         <h4 className="text-light text-5">Live in Dhaka, Bangladesh.</h4>
         <Button className=" mt-4"><a href="#contact">Hire Me</a></Button>
       </div>
